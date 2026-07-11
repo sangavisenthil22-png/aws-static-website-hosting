9cfb5f33014a03f0c17f5508c7c0f3c7491efc18
sangavisenthil22-png <sangavisenthil22@gmail.com>
2026-07-11 11:01:07 +0530

    Delete user-data.sh

--- user-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dnf update -y
-dnf install -y httpd
-systemctl enable httpd
-systemctl start httpd
-
-echo "<h1>Apache Web Server is Running!</h1><p>Created using EC2 User Data.</p>" > /var/www/html/index.html
